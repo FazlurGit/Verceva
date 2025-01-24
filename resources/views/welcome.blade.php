7319e4a2858796de8a51b0c88ad5ae279b2cd23d
FazlurGit <fazlur.rahman@mhs.itenas.ac.id>
2025-01-24 18:57:33 +0700

    Update ajul 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,12 +12,17 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400;700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+     <!-- CloudFlare Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 </head>
 <body>
     <div class="hero">
         
                         <!-- navbar  start -->
-        <nav>
+         <nav>
             <a href=""><h2 class="logo">Verc<span>eva</span>.</h2></a>
             <ul>
                 <li><a href="#">Home</a></li>
@@ -38,13 +43,13 @@
         <div class="slider">
         <div class="slides">
             <div class="slide">
-                <img src="images/hero2.jpeg" alt="Image 1">
+                <img src="images/Cover3.jpg" alt="Image 1">
             </div>
             <div class="slide">
-                <img src="images/hero.jpeg" alt="Image 2">
+                <img src="images/Cover2.jpg" alt="Image 2">
             </div>
             <div class="slide">
-                <img src="images/hero3.jpg" alt="Image 3">
+                <img src="images/Cover4.jpg" alt="Image 3">
             </div>
         </div>
         <button class="arrow arrow-left">&#8249;</button>
